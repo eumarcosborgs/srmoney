fcb6657f826af26fd065aeaea60e0ec4d704e6f6
Marcos Paulo <eumarcosborgs@gmail.com>
2022-01-10 14:44:38 -0400

    fix: ensureAuthenticated middleware

--- backend/src/shared/infra/http/middlewares/ensureAuthenticated.ts
+++ backend/src/shared/infra/http/middlewares/ensureAuthenticated.ts
@@ -15,7 +15,7 @@ export function ensureAuthenticated(
   response: Response,
   next: NextFunction,
 ): void {
-  const authHeader = request.body.authorization;
+  const authHeader = request.headers.authorization;
 
   if (!authHeader) {
     throw new AppError('JWT token is missing', 401);
